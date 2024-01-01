3cacb9b756ca2a906339cce08f2b9026081c8d91
Administrator <admin@example.com>
2024-01-01 10:29:09 +0100

    first commit

--- wol.py
+++ wol.py
@@ -1,3 +1,8 @@
 from wakeonlan import send_magic_packet
 # dest mac-address, source-interface
-send_magic_packet('70.f3.95.01.4a.4f', interface='192.168.178.31')
+#
+# networklab
+# send_magic_packet('70.f3.95.01.4a.4f', interface='192.168.178.31')
+#
+# pve
+send_magic_packet('64.00.6a.73.44.d9', interface='192.168.178.31')
